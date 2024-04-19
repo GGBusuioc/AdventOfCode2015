fc9e1debfaf0aec3a3f8d8c666fe17c38e0a8b1d
George-Gabriel Busuioc <31717162+GGBusuioc@users.noreply.github.com>
2024-04-19 10:49:43 +0000

    Day 3 Part 1 - Finished

--- day-3/part1.js
+++ day-3/part1.js
@@ -1,4 +1,28 @@
 import fs from "fs"
 
+// each time santa moves -> create new key in the dict -> 
+// key representing the coordonates -> value is the number of times the cordonates were visited 
+// [n, s, w, e] starting at [0, 0, 0, 0] = 1 OR [dx, dy] -> [0, 0] = 1 (1 representing a house being visited)
+// number of entries in the dict is the number of houses
 
-const houses = fs.readFileSync("./data.txt", "utf8");
+let dict = {}
+
+let dx = 0
+let dy = 0
+
+dict[[dx, dy]] = 1
+
+fs.readFileSync("./data.txt", "utf8").split('').forEach((character) => {
+    switch (character) {
+        case '^' : dy++; break
+        case 'v' : dy--; break
+        case '>' : dx++; break
+        case '<' : dx--; break
+    }
+    
+    // mark the house as visited
+    dict[[dx, dy]] = 1
+})
+
+const numberOfHousesVisited = Object.keys(dict).length
+console.log(numberOfHousesVisited)
