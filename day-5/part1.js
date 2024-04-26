e764c503d4f2e63d8f8c26ee8354984a48803ed6
George-Gabriel Busuioc <31717162+GGBusuioc@users.noreply.github.com>
2024-04-26 11:12:24 +0000

    Day 5 - Part 1 Update Progress

--- day-5/part1.js
+++ day-5/part1.js
@@ -1,5 +1,23 @@
 import fs from "fs"
 
-fs.readFileSync("./data.txt", "utf8").split('').forEach((string) => {
-    console.log(string)
+// It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+
+// It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+// It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+
+const regex1 = new RegExp('([aeiou].*){3,}');
+const regex2 = new RegExp('(.)\\1');
+const regex3 = /(ab|cd|pq|xy)/gm
+
+let countNiceStrings = 0
+
+fs.readFileSync("./data.txt", "utf8")
+    .split(/\r?\n/)
+    .forEach((string) => {
+    if (regex1.test(string) && regex2.test(string) && !regex3.test(string)  ) {
+        countNiceStrings += 1
+        console.log(string)
+    }
 })
+console.log(countNiceStrings)
+
